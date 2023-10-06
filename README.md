b80d4f3ee6945c612e9abe7bfabd4d385d4aa54f
Shinnen <phamminhdang135@gmail.com>
2023-10-07 01:35:43 +0700

    update all code

---
+++ README.md
@@ -0,0 +1,25 @@
+# CosmJs Part For Checkers Blockchain
+
+This project contains the elements, messages, scripts and GUI of the Checkers blockchain.
+
+## Branches progression
+
+There are a number of branches. The purpose of these branches is to show how the project advances as it is prepared. You can do diffs between to see the differences.
+
+* [`start`](https://github.com/cosmos/academy-checkers-ui/tree/start)
+* [`generated`](https://github.com/cosmos/academy-checkers-ui/tree/generated) [diff](https://github.com/cosmos/academy-checkers-ui/compare/start...generated)
+* [`stargate`](https://github.com/cosmos/academy-checkers-ui/tree/stargate) [diff](https://github.com/cosmos/academy-checkers-ui/compare/generated...stargate)
+* [`signing-stargate`](https://github.com/cosmos/academy-checkers-ui/tree/signing-stargate) [diff](https://github.com/cosmos/academy-checkers-ui/compare/stargate...signing-stargate)
+* [`unwired-gui`](https://github.com/cosmos/academy-checkers-ui/tree/unwired-gui) [diff](https://github.com/cosmos/academy-checkers-ui/compare/signing-stargate...unwired-gui)
+* [`gui`](https://github.com/cosmos/academy-checkers-ui/tree/gui) [diff](https://github.com/cosmos/academy-checkers-ui/compare/unwired-gui...gui)
+* [`server-indexing`](https://github.com/cosmos/academy-checkers-ui/tree/server-indexing) [diff](https://github.com/cosmos/academy-checkers-ui/compare/gui...server-indexing)
+
+## V1 branches progression
+
+* [`start`](https://github.com/cosmos/academy-checkers-ui/tree/start)
+* [`v1-generated`](https://github.com/cosmos/academy-checkers-ui/tree/v1-generated) [diff](https://github.com/cosmos/academy-checkers-ui/compare/start...v1-generated)
+* [`v1-stargate`](https://github.com/cosmos/academy-checkers-ui/tree/v1-stargate) [diff](https://github.com/cosmos/academy-checkers-ui/compare/v1-generated...v1-stargate)
+* [`v1-signing-stargate`](https://github.com/cosmos/academy-checkers-ui/tree/v1-signing-stargate) [diff](https://github.com/cosmos/academy-checkers-ui/compare/v1-stargate...v1-signing-stargate)
+* [`v1-unwired-gui`](https://github.com/cosmos/academy-checkers-ui/tree/v1-unwired-gui) [diff](https://github.com/cosmos/academy-checkers-ui/compare/v1-signing-stargate...v1-unwired-gui)
+* [`gui`](https://github.com/cosmos/academy-checkers-ui/tree/v1-gui) [diff](https://github.com/cosmos/academy-checkers-ui/compare/v1-unwired-gui...v1-gui)
+* [`v1-server-indexing`](https://github.com/cosmos/academy-checkers-ui/tree/v1-server-indexing) [diff](https://github.com/cosmos/academy-checkers-ui/compare/v1-gui...v1-server-indexing)
---
+++ src/types/checkers/messages.ts
@@ -0,0 +1,63 @@
+import { EncodeObject, GeneratedType } from "@cosmjs/proto-signing"
+import {
+    MsgCreateGame,
+    MsgCreateGameResponse,
+    MsgPlayMove,
+    MsgPlayMoveResponse,
+} from "../generated/cosmoscheckers/tx"
+
+export const typeUrlMsgCreateGame = "/perfogic.cosmoscheckers.cosmoscheckers.MsgCreateGame"
+export const typeUrlMsgCreateGameResponse = "/perfogic.cosmoscheckers.cosmoscheckers.MsgCreateGameResponse"
+export const typeUrlMsgPlayMove = "/perfogic.cosmoscheckers.cosmoscheckers.MsgPlayMove"
+export const typeUrlMsgPlayMoveResponse = "/perfogic.cosmoscheckers.cosmoscheckers.MsgPlayMoveResponse"
+
+export const checkersTypes: ReadonlyArray<[string, GeneratedType]> = [
+    [typeUrlMsgCreateGame, MsgCreateGame],
+    [typeUrlMsgCreateGameResponse, MsgCreateGameResponse],
+    [typeUrlMsgPlayMove, MsgPlayMove],
+    [typeUrlMsgPlayMoveResponse, MsgPlayMoveResponse],
+]
+
+export interface MsgCreateGameEncodeObject extends EncodeObject {
+    readonly typeUrl: "/perfogic.cosmoscheckers.cosmoscheckers.MsgCreateGame"
+    readonly value: Partial<MsgCreateGame>
+}
+
+export function isMsgCreateGameEncodeObject(
+    encodeObject: EncodeObject,
+): encodeObject is MsgCreateGameEncodeObject {
+    return (encodeObject as MsgCreateGameEncodeObject).typeUrl === typeUrlMsgCreateGame
+}
+
+export interface MsgCreateGameResponseEncodeObject extends EncodeObject {
+    readonly typeUrl: "/perfogic.cosmoscheckers.cosmoscheckers.MsgCreateGameResponse"
+    readonly value: Partial<MsgCreateGameResponse>
+}
+
+export function isMsgCreateGameResponseEncodeObject(
+    encodeObject: EncodeObject,
+): encodeObject is MsgCreateGameResponseEncodeObject {
+    return (encodeObject as MsgCreateGameResponseEncodeObject).typeUrl === typeUrlMsgCreateGameResponse
+}
+
+export interface MsgPlayMoveEncodeObject extends EncodeObject {
+    readonly typeUrl: "/perfogic.cosmoscheckers.cosmoscheckers.MsgPlayMove"
+    readonly value: Partial<MsgPlayMove>
+}
+
+export function isMsgPlayMoveEncodeObject(
+    encodeObject: EncodeObject,
+): encodeObject is MsgPlayMoveEncodeObject {
+    return (encodeObject as MsgPlayMoveEncodeObject).typeUrl === typeUrlMsgPlayMove
+}
+
+export interface MsgPlayMoveResponseEncodeObject extends EncodeObject {
+    readonly typeUrl: "/perfogic.cosmoscheckers.cosmoscheckers.MsgPlayMoveResponse"
+    readonly value: Partial<MsgPlayMoveResponse>
+}
+
+export function isMsgPlayMoveResponseEncodeObject(
+    encodeObject: EncodeObject,
+): encodeObject is MsgPlayMoveResponseEncodeObject {
+    return (encodeObject as MsgPlayMoveResponseEncodeObject).typeUrl === typeUrlMsgPlayMoveResponse
+}
